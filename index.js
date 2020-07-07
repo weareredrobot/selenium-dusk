2d245bac4db22f5219d791ca4e258fb9b0fef88c
Ricardo Graça <ricardo.graca17@bathspa.ac.uk>
2020-07-07 16:56:48 +0100

    Read command line arguments and get templates

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
+const files = require("./fileSystem");
+
 require('yargs')
-    .command('$0 [name]', 'say hello', (yargs) => {
+    .command('$0 [path]', 'Convert Selenium test to Dusk test', (yargs) => {
         yargs
-            .positional('name', {
-                describe: 'hello\'s target',
-                default: 'world'
+            .positional('path', {
+                describe: 'Path to Selenium test',
+                default: ''
             })
-            .option('times', {
-                alias: 't',
-                type: 'number',
-                default: 1,
-                description: 'number of times to say hello'
+            .option('output', {
+                alias: 'o',
+                type: 'string',
+                default: '/',
+                description: 'Output path of Dusk tests'
             })
-    }, (argv) => {
-        for (let i = 0;i < argv.times; i++) {
-            console.log(`Hello ${argv.name}!`)
-        }
+    }, (argv) => 
+    {
+        var input = argv.path;
+        var ouput = argv.output;
+
+        files.read(input, function(data){
+            if(data["err"]){
+                console.log("Error reading the file (possibly wrong input)")
+            }
+            else{
+                console.log("Received input file");
+
+                var seleniumFile = JSON.parse(data["data"]);
+
+                files.read('./template.txt', function(data){
+                        var template = data.data;
+                });
+
+                for(var i = 0; i < seleniumFile.tests.length; i++){
+                    console.log(seleniumFile.tests[i].name);
+
+                    files.read('./templateFunction.txt', function(data){
+                        var template = data.data;
+                        template = template.replace("{{name}}", "test");
+                        console.log(template);
+                    })
+                }
+            }
+        })
+
+        
     })
     .argv
