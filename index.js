226dfef8c4baf0d7495032638f27a0ab0428a0ab
Ricardo Graça <ricardo.graca17@bathspa.ac.uk>
2020-07-08 23:17:27 +0100

    Read templates, convert code and generate php file

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const files = require("./src/fileSystem");
+const   files = require("./src/fileSystem"),
+        convert = require('./src/convert');
+
+var seleniumFile, templateFile, templateFunctionFile, templateArrayFunctionFile = [], counter = 0;
 
 require('yargs')
     .command('$0 [path]', 'Convert Selenium test to Dusk test', (yargs) => {
@@ -27,22 +30,43 @@ require('yargs')
 
                 var seleniumFile = JSON.parse(data["data"]);
 
-                files.read('./template.txt', function(data){
-                        var template = data.data;
+                files.read('./src/templates/template.txt', function(data){
+                    var templateFile = data.data;
+
+                    files.read('./src/templates/templateFunction.txt', function(data){
+                        var templateFunctionFile = data.data;
+
+                        for(var i = 0; i < seleniumFile.tests.length; i++){
+                            convert.commands(seleniumFile.tests[i].commands, i)
+                            .then(function(data){
+                                var tempTemplateFunctionFile = templateFunctionFile;
+                                var functionName = seleniumFile.tests[data["counter"]].name.replace(" ", "");
+
+                                tempTemplateFunctionFile = tempTemplateFunctionFile.replace("{{name}}", functionName);
+                                tempTemplateFunctionFile = tempTemplateFunctionFile.replace("{{function}}", data["function"]);
+
+                                templateArrayFunctionFile.push(tempTemplateFunctionFile);
+                            })
+                            .then(function(){
+                                counter++;
+
+                                if(counter >= seleniumFile.tests.length){
+                                    templateFile = templateFile.replace("{{method}}", templateArrayFunctionFile.join(''));
+                                    templateFile = templateFile.replace("{{testClassName}}", seleniumFile.name)
+
+                                    files.write(output + "/" + seleniumFile.name + ".php", templateFile, function(status){
+                                        if(status){
+                                            console.log("Test converted");
+                                        } else {
+                                            console.log("Failed to convert test");
+                                        }
+                                    })
+                                }    
+                            })
+                        }
+                    });
                 });
-
-                for(var i = 0; i < seleniumFile.tests.length; i++){
-                    console.log(seleniumFile.tests[i].name);
-
-                    files.read('./templateFunction.txt', function(data){
-                        var template = data.data;
-                        template = template.replace("{{name}}", "test");
-                        console.log(template);
-                    })
-                }
             }
         })
-
-        
     })
     .argv
