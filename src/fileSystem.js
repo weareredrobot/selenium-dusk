d0c3edd34a79d01a2fe49989105b71bf42ba3a14
Gary Renes <gaz@red-robot.co.uk>
2020-07-10 12:21:55 +0100

    Minor tidy

--- src/fileSystem.js
+++ src/fileSystem.js
@@ -1,32 +1,27 @@
 const fs = require('fs');
 
 module.exports = {
-    read(path, callback){
-        fs.readFile(path, 'utf8', function(err, data){
-            if(err){
-                callback({err: "File not found"});
-            }
-            else
-            {
-                callback({data: data});
-            }
-        });
-    },
 
-    write (path, data, callback){
-        fs.writeFile(path, data, function(err){
-            if(err) callback(0);
-            callback(1);
-        })
-    },
+  read: (path, callback) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        callback({err: "File not found"});
+      } else {
+        callback({data: data});
+      }
+    });
+  },
 
-    exists (path, callback){
-        fs.access(path, fs.constants.F_OK, (err) => {
-            if(err){
-                callback(0);
-            } else {
-                callback(1);
-            }
-        })
-    }
+  write: (path, data, callback) => {
+    fs.writeFile(path, data, (err) => {
+      if(err) callback(0);
+      callback(1);
+    })
+  },
+
+  exists: (path, callback) => {
+    fs.access(path, fs.constants.F_OK, (err) => {
+      callback(!err)
+    });
+  }
 }
